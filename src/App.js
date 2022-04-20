4c3659d4bf674fab85497c91019929fe2ba3c11e
Yafi <tahzibislamyafi@gmail.com>
2022-04-20 16:33:43 -0600

    food items are displayed properly

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./Data";
 
-const allCategories = ['all', ... new Set(items.map((item)=>item.category))]; // Grab all unique categories
+const allCategories = ['all', ...new Set(items.map((item)=>item.category))]; // Grab all unique categories
 
 function App() {
   const [foodItems, setFoodItems] = useState(items);  // Initialize food items with data from Data.js
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) =>{
+    if (category==='all'){
+      setFoodItems(items);
+      return;
+    }
     const newItems = items.filter((item)=>item.category===category);  // Filter food items based on the category 
     setFoodItems(newItems);
   }
   return(
+    // Display the food items and their information
   <main>
     <section className="menu section">
       <div className="title">
         <h2>Today's menu</h2>
         <div className="underline"></div>
-        <Categories filterItems={filterItems}></Categories>
+        <Categories categories={categories} filteritems={filterItems}></Categories>
         <Menu items={foodItems}></Menu>
       </div>
     </section>
--- src/Categories.js
+++ src/Categories.js
@@ -1,5 +1,20 @@
-export default function Categories(props){
-    return(
-        <div>Categories</div>
-    );
-} 
+const Categories = ({ categories, filteritems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filteritems(category)} // Categorize the items based on the category clicked
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
