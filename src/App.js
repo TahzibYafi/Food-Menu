9ea6a3028f216412de7a27f119caa71a0e32c586
Yafi <tahzibislamyafi@gmail.com>
2022-04-20 12:16:30 -0600

    menu is formatted properly, but doesn't display images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './Data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./Data";
 
 function App() {
+  const [foodItems, setFoodItems] = useState(items);  // Initialize food items with data from Data.js
+  const [categories, setCategories] = useState([]);
 
 
-  return (
-<div>
-  Food Menu
-</div>
-  );
+  return(
+  <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>Today's menu</h2>
+        <div className="underline"></div>
+        <Categories></Categories>
+        <Menu items={foodItems}></Menu>
+      </div>
+    </section>
+  </main>);
 }
 
 export default App;
---
+++ src/Menu.js
@@ -0,0 +1,17 @@
+const Menu = ({items}) =>{
+    return(
+        <div className="section-center">
+            {items.map((menuItem)=>
+            {
+                const {id, title, img, desc} = menuItem;
+                return (
+                    <article key={id} className="menu-item">
+                        <img src={img} alt={title} className='photo'></img>
+                    </article>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Menu;
